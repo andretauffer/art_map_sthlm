fce75af774cae2c9c2058a6b5f130ae0f606b30a
andretauffer <andregiraotauffer@gmail.com>
2019-10-29 20:15:15 +0100

    Deleted some unused code in app

--- server/app.js
+++ server/app.js
@@ -74,39 +74,9 @@ app.get("/deleteUsers", async (req, res) => {
   res.end();
 });
 
+// Routes
+
 app.use("/api/login", login);
-
-// app.post("/api/login", async (req, res) => {
-//   console.log("req body", req.body);
-//   let user = await pool.query("SELECT * FROM Users WHERE username = $1", [
-//     req.body.username
-//   ]);
-//   if (user.rows[0].password === req.body.password) {
-//     const token = jwt.sign(
-//       {
-//         data: user.rows[0].id
-//       },
-//       "secret",
-//       { expiresIn: 60 * 60 }
-//     );
-//     const retVal = { login: 1, id: token, name: user.rows[0].name };
-//     res.send(JSON.stringify(retVal));
-//   } else {
-//     const retVal = { login: 0, status: "Invalid Password" };
-//     res.send(JSON.stringify(retVal));
-//   }
-// });
-
-// app.get('/worksT', async (req, res) => {
-//     await client.query(`create table if not exists Users (
-//         id serial primary key,
-//         username varchar not null unique,
-//         password varchar not null,
-//         name varchar not null
-//   );`)
-
-//     res.end();
-//   });
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "art_map_sthlm/build")));
@@ -116,7 +86,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-// connect();
 const port = process.env.PORT || 5000;
 app.listen(port);
 
